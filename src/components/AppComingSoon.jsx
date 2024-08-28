94d8e850cb534cba9f820a3f8e42ff29cd5564d1
nayeem101 <inayeem044@gmail.com>
2024-08-28 22:05:03 +0600

    new app images added in app coming soon section

--- src/components/AppComingSoon.jsx
+++ src/components/AppComingSoon.jsx
@@ -1,6 +1,6 @@
-import AppImage1 from "../assets/images/app_image1.png";
-import AppImage2 from "../assets/images/app_image2.png";
-import AppImage3 from "../assets/images/app_image3.png";
+import AppImage1 from "../assets/images/app-img-1.jpg";
+import AppImage2 from "../assets/images/app-img-2.jpg";
+import AppImage3 from "../assets/images/app-img-3.jpg";
 import SectionHeader from "./shared/SectionHeader";
 
 import { useTranslation } from "react-i18next";
@@ -15,9 +15,9 @@ const AppComingSoon = () => {
       </div>
       <div className="max-w-screen-lg mx-auto mb-8">
         <div className="w-full flex justify-start md:justify-center gap-3 overflow-x-scroll md:overflow-x-hidden">
-          <img src={AppImage1} alt="UkilZone App home" className="w-48 md:w-52 lg:w-64 xl:w-72" />
-          <img src={AppImage3} alt="UkilZone App lawyers" className="w-48 md:w-52 lg:w-64 xl:w-72" />
-          <img src={AppImage2} alt="UkilZone App services" className="w-48 md:w-52 lg:w-64 xl:w-72" />
+          <img src={AppImage1} alt="UkilZone App home" className="w-56 md:w-64 lg:w-72 xl:w-80" />
+          <img src={AppImage3} alt="UkilZone App lawyers" className="w-56 md:w-64 lg:w-72 xl:w-80" />
+          <img src={AppImage2} alt="UkilZone App services" className="w-56 md:w-64 lg:w-72 xl:w-80" />
         </div>
       </div>
     </section>
--- src/components/shared/SectionHeader.jsx
+++ src/components/shared/SectionHeader.jsx
@@ -5,7 +5,7 @@ const SectionHeader = ({ title }) => {
   return (
     <div className="max-w-screen-md mb-6 lg:mb-8 mx-auto flex justify-center gap-6 items-center">
       <FaScaleBalanced className="w-10 h-10 text-gray-900 dark:text-gray-100" />
-      <h2 className="text-4xl [line-height:46px] text-center tracking-tight font-bold text-gray-900 dark:text-white header-title-border">
+      <h2 className="text-3xl lg:text-4xl text-center tracking-tight font-bold text-gray-900 dark:text-white header-title-border">
         {title}
       </h2>
       <FaScaleBalanced className="w-10 h-10 text-gray-900 dark:text-gray-100" />
